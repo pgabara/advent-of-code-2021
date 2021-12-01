94262f225ae10cb50ea4cdaef274aedebd8412ec
Piotr Gabara <piotr.x.gabara@gmail.com>
2021-12-01 18:09:35 +0100

    Day 1 - Refactoring

--- src/d01.rs
+++ src/d01.rs
@@ -1,13 +1,9 @@
-pub fn count_number_of_times_depth_measurement_increases(data: &Vec<usize>) -> usize {
-    count_depth_measurement_increases(data)
-}
-
 pub fn count_number_of_times_depth_measurement_window_increases(data: &Vec<usize>) -> usize {
     let data: Vec<usize> = data.windows(3).map(|x| x.iter().sum()).collect();
-    count_depth_measurement_increases(&data)
+    count_number_of_times_depth_measurement_increases(&data)
 }
 
-fn count_depth_measurement_increases(data: &Vec<usize>) -> usize {
+pub fn count_number_of_times_depth_measurement_increases(data: &Vec<usize>) -> usize {
     data.windows(2).fold(0, |acc, x| {
         if let &[a, b] = x {
             if b > a { acc + 1 } else { acc }
